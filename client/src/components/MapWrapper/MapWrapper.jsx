6a6fec74f3b5c607229d6fb10224f98700cd5feb
chrispylam <christopherwlam@gmail.com>
2021-03-12 15:15:59 -0800

    No functionality timeslider on the map

--- client/src/components/MapWrapper/MapWrapper.jsx
+++ client/src/components/MapWrapper/MapWrapper.jsx
@@ -11,6 +11,18 @@ import { states_hawaii } from '../../res/hawaii.js';
 import { eachState, eachStatesCounties,defaultColors,covidColors } from '../../utils/data-utils';
 import { useLocation } from 'react-router-dom';
 
+import Nouislider from "nouislider-react";
+import "nouislider/distribute/nouislider.css";
+
+function timestamp(str) {
+    return new Date(str).getTime();
+}
+const years = ["2020", "2021"];
+const months_short = [
+    "Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct","Nov", "Dec"
+];
+const monthVals = months_short.map(month=>Date.parse(month+" 1, 2020"))
+
 
 // move to map res utils
 const usaCentre = [38., -96.], usaBounds = [[18., -135.], [52., -60.]];
@@ -34,7 +46,18 @@ const MapWrapper = (props) => {
 
   return (
     <div id="MapWrapper">
-
+      <div id="timeslider">
+        <Nouislider behaviour='tap-drag'
+                    connect
+                    range={{
+                        min: timestamp('Jan 1,2020'),
+                        max: timestamp(Date.now())
+                    }}
+                    direction='ltr'
+                    step={24 * 60 * 60 * 1000}
+                    start={[timestamp('Jan 1,2020'), timestamp(Date.now())]}
+                    />
+      </div>
       <Map id="USA" ref={props.mapRef} maxBounds={worldBounds} minZoom={2} zoomSnap={0.25} center={usaCentre} zoom={4.5} zoomEnd={props.updateZoom}>
         <TileLayer bounds={worldBounds} attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png" />
@@ -45,6 +68,7 @@ const MapWrapper = (props) => {
           <GeoJSON ref={props.hawaiiRef} data={states_hawaii} onEachFeature={(feature, layer) => eachState(feature, layer, props.data, 100, props.updateState,theColors)} />
           <GeoJSON data={usa} onEachFeature={(feature, layer) => { layer.setStyle({stroke: 0.3, color: '#777777'})}} />
         <Legend colors={theColors} covid={covidFlag} />
+        
         {props.children}
       </Map>
     </div>
